7099be4d2514d86d220c7cfcbb06abf373ac9b76
MrazTevin <millatevin@gmail.com>
2022-10-02 09:46:45 +0300

    m: comment out code

--- js/tax.js
+++ js/tax.js
@@ -1,16 +1,16 @@
-// function calculateTax() {
-//     var salary = parseFloat(document.getElementById("fname").value);
-//     var tax = amount * 0.1;
-//     var total = tax + amount;
-//     document.getElementById("result").innerHTML = "tax = " + tax + " and total is " + total;
+a// // function calculateTax() {
+// //     var salary = parseFloat(document.getElementById("fname").value);
+// //     var tax = amount * 0.1;
+// //     var total = tax + amount;
+// //     document.getElementById("result").innerHTML = "tax = " + tax + " and total is " + total;
 
-wil
-    document.getElementById("entry").addEventListener(
-        "click",calculateTax);
-}
-
+// wil
 //     document.getElementById("entry").addEventListener(
 //         "click",calculateTax);
-
 // }
- development
+
+// //     document.getElementById("entry").addEventListener(
+// //         "click",calculateTax);
+
+// // }
+//  development
